275cf8d55696f52e99f9d982a04af6a47b21a55e
zeroto99 <myv_01@naver.com>
2022-05-29 23:44:23 +0900

    🚧 마크업, 스타일링 진행 중

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,10 +1,32 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 import Header from '../components/Header';
+import ProjectList from '../data/ProjectList';
+
+const Main = styled.main`
+  height: 100vh;
+  padding-top: 10rem;
+`;
 
 const Section = styled.section`
-  height: 100vh;
-  margin: 0 auto;
+  display: flex;
+  gap: 10rem;
+  margin-bottom: 2rem;
+`;
+
+const Mockup = styled.article`
+  border: 1px solid black;
+  width: 50%;
+  height: 40rem;
+`;
+
+const Description = styled.article`
+  border: 1px solid black;
+  width: 50%;
+`;
+
+const DesList = styled.ul`
+
 `;
 
 const Test = styled.div`
@@ -19,31 +41,40 @@ const Height = styled.div`
 `;
 
 const Project = () => {
-  const [position, setPosition] = useState(0);
+  // const [position, setPosition] = useState(0);
 
-  const onScroll = () => {
-    setPosition(window.scrollY);
-  }
+  // const onScroll = () => {
+  //   setPosition(window.scrollY);
+  // }
 
-  // 스크롤 될 때마다 onScroll 함수가 실행되서 scrollY의 값이 position state에 업데이트된다.
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll);
-    // 스크롤 될 때마다 이벤트가 발생하면 메모리 누수가 발생하기 때문에 컴포넌트가 언마운트 되기 직전에 이벤트를 제거한다. 
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, []);
+  // // 스크롤 될 때마다 onScroll 함수가 실행되서 scrollY의 값이 position state에 업데이트된다.
+  // useEffect(() => {
+  //   window.addEventListener('scroll', onScroll);
+  //   // 스크롤 될 때마다 이벤트가 발생하면 메모리 누수가 발생하기 때문에 컴포넌트가 언마운트 되기 직전에 이벤트를 제거한다. 
+  //   return () => {
+  //     window.removeEventListener('scroll', onScroll);
+  //   };
+  // }, []);
+  // {/* <h1>프로젝트입니다</h1>
+  // <Test style={{
+  //   transform: `translateX(${position}px)`
+  // }} />
+  // <Height /> */}
 
   return (
     <>
       <Header />
-      <Section>
-        <h1>프로젝트입니다</h1>
-        <Test style={{
-          transform: `translateX(${position}px)`
-        }} />
-        <Height />
-      </Section>
+        <Main>
+          {ProjectList.map((item) => (
+            <Section>
+              <Mockup style={{background: `url(${item.img})`}}>
+              </Mockup>
+              <Description>
+                <h2>{item.title}</h2>
+              </Description>
+            </Section>
+          ))}
+        </Main>
     </>
   )
 }
