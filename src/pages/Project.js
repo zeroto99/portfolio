2c82ca0dbf735166b345d0fb0da05ae4a6520cae
zeroto99 <myv_01@naver.com>
2022-05-30 21:42:16 +0900

    🚧 마크업, 스타일링 진행 중

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -5,27 +5,54 @@ import ProjectList from '../data/ProjectList';
 
 const Main = styled.main`
   height: 100vh;
-  padding-top: 10rem;
+  padding-top: 20rem;
 `;
 
 const Section = styled.section`
   display: flex;
   gap: 10rem;
-  margin-bottom: 2rem;
+  margin-bottom: 12rem;
 `;
 
 const Mockup = styled.article`
-  border: 1px solid black;
   width: 50%;
   height: 40rem;
+  overflow: hidden;
+
+  img {
+    transform: scale(1.5);
+  }
 `;
 
 const Description = styled.article`
-  border: 1px solid black;
   width: 50%;
 `;
 
-const DesList = styled.ul`
+const TxtWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h2 {
+    font-size: 2rem;
+    font-weight: 700;
+  }
+`;
+
+const MainUl = styled.ul`
+  margin-top: 2rem;
+`;
+
+const MainLi = styled.li`
+  margin-bottom: 3rem;
+`;
+
+const SubUl = styled.ul`
+  line-height: 2.5rem;
+  margin-top: 1rem;
+`;
+
+const SubLi = styled.li`
 
 `;
 
@@ -63,14 +90,52 @@ const Project = () => {
 
   return (
     <>
-      <Header />
         <Main>
           {ProjectList.map((item) => (
-            <Section>
-              <Mockup style={{background: `url(${item.img})`}}>
+            <Section key={item.key}>
+              <Mockup>
+                <img src= {item.img} alt="목업 이미지" />
               </Mockup>
               <Description>
-                <h2>{item.title}</h2>
+                <TxtWrapper>
+                  <h2>{item.title}</h2>
+                  <small>{item.date}</small>
+                </TxtWrapper>
+                <MainUl>
+                  <MainLi>✨소개
+                    <SubUl>
+                      <SubLi>{item.inf}</SubLi>
+                    </SubUl>
+                  </MainLi>
+                  <MainLi>✨작업 방식
+                    <SubUl>
+                      {item.way.split('/').map((item, idx) => (
+                        <SubLi key={idx}>- {item}</SubLi>
+                      ))}
+                    </SubUl>
+                  </MainLi>
+                  <MainLi>✨역할
+                    <SubUl>
+                      {item.role.split('/').map((item, idx) => (
+                        <SubLi key={idx}>- {item}</SubLi>
+                      ))}
+                    </SubUl>
+                  </MainLi>
+                  {
+                    item.note !== '' ? 
+                    ( 
+                      <MainLi>✨노트
+                        <SubUl>
+                          <SubLi>{item.note}</SubLi>
+                        </SubUl>
+                      </MainLi> 
+                    ) 
+                    : null
+                  }
+                  <MainLi>
+                    <a href={item.link} target='_blank'>✨보러가기!</a>
+                  </MainLi>
+                </MainUl>
               </Description>
             </Section>
           ))}
