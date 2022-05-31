c9b416f8e41d318a5960a686acbb702eb6ec8bff
zeroto99 <myv_01@naver.com>
2022-05-31 23:59:45 +0900

    🚧  마크업 진행 중

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,16 +2,36 @@ import styled from 'styled-components';
 import Header from '../components/Header';
 
 const Section = styled.section`
-  height: 100vh;
-  margin: 0 auto;
+  padding-top: 7rem;
+`;
+
+const Article = styled.article`
+
+`;
+
+const Tit = styled.h2`
+
 `;
 
 const Resume = () => {
   return (
     <>
-      <Header />
       <Section>
-        <h1>이력서입니다</h1>
+        <Article>
+          <Tit>소개</Tit>
+        </Article>
+        <Article>
+          <Tit>스킬</Tit>
+        </Article>
+        <Article>
+          <Tit>프로젝트</Tit>
+        </Article>
+        <Article>
+          <Tit>활동</Tit>
+        </Article>
+        <Article>
+          <Tit>수료 및 자격증</Tit>
+        </Article>
       </Section>
     </>
   )
