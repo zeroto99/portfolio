474496479a560b01e9f2bf88f598f589eb243709
zeroto99 <myv_01@naver.com>
2022-06-01 23:54:01 +0900

    🚧 스크롤 시 요소 fade in 효과 작업 중

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Header from '../components/Header';
 import ProjectList from '../data/ProjectList';
 
@@ -18,6 +18,8 @@ const Mockup = styled.article`
   width: 50%;
   height: 40rem;
   overflow: hidden;
+  transform: translateX(-44px);
+  opacity: 0;
 
   img {
     transform: scale(1.5);
@@ -68,78 +70,71 @@ const Height = styled.div`
 `;
 
 const Project = () => {
-  // const [position, setPosition] = useState(0);
 
-  // const onScroll = () => {
-  //   setPosition(window.scrollY);
-  // }
+  const targets = useRef([]);
 
-  // // 스크롤 될 때마다 onScroll 함수가 실행되서 scrollY의 값이 position state에 업데이트된다.
-  // useEffect(() => {
-  //   window.addEventListener('scroll', onScroll);
-  //   // 스크롤 될 때마다 이벤트가 발생하면 메모리 누수가 발생하기 때문에 컴포넌트가 언마운트 되기 직전에 이벤트를 제거한다. 
-  //   return () => {
-  //     window.removeEventListener('scroll', onScroll);
-  //   };
-  // }, []);
-  // {/* <h1>프로젝트입니다</h1>
-  // <Test style={{
-  //   transform: `translateX(${position}px)`
-  // }} />
-  // <Height /> */}
+  useEffect(() => {
+    console.log(targets.current);
+    targets.current.forEach((ele) => console.log(ele.offsetTop));
+  }, []);
+
 
   return (
     <>
-        <Main>
-          {ProjectList.map((item) => (
-            <Section key={item.key}>
-              <Mockup>
-                <img src= {item.img} alt="목업 이미지" />
-              </Mockup>
-              <Description>
-                <TxtWrapper>
-                  <h2>{item.title}</h2>
-                  <small>{item.date}</small>
-                </TxtWrapper>
-                <MainUl>
-                  <MainLi>✨소개
-                    <SubUl>
-                      <SubLi>{item.inf}</SubLi>
-                    </SubUl>
-                  </MainLi>
-                  <MainLi>✨작업 방식
-                    <SubUl>
-                      {item.way.split('/').map((item, idx) => (
-                        <SubLi key={idx}>- {item}</SubLi>
-                      ))}
-                    </SubUl>
-                  </MainLi>
-                  <MainLi>✨역할
-                    <SubUl>
-                      {item.role.split('/').map((item, idx) => (
-                        <SubLi key={idx}>- {item}</SubLi>
-                      ))}
-                    </SubUl>
-                  </MainLi>
-                  {
-                    item.note !== '' ? 
-                    ( 
-                      <MainLi>✨노트
-                        <SubUl>
-                          <SubLi>{item.note}</SubLi>
-                        </SubUl>
-                      </MainLi> 
-                    ) 
-                    : null
-                  }
-                  <MainLi>
-                    <a href={item.link} target='_blank'>✨보러가기!</a>
-                  </MainLi>
-                </MainUl>
-              </Description>
-            </Section>
-          ))}
-        </Main>
+      <Main>
+        {ProjectList.map((item, idx) => (
+          <Section key={item.key}>
+            <Mockup 
+              ref={
+                (ele) => {targets.current[idx] = ele}
+              }
+            >
+              <img src= {item.img} alt="목업 이미지" />
+            </Mockup>
+            <Description>
+              <TxtWrapper>
+                <h2>{item.title}</h2>
+                <small>{item.date}</small>
+              </TxtWrapper>
+              <MainUl>
+                <MainLi>✨소개
+                  <SubUl>
+                    <SubLi>{item.inf}</SubLi>
+                  </SubUl>
+                </MainLi>
+                <MainLi>✨작업 방식
+                  <SubUl>
+                    {item.way.split('/').map((item, idx) => (
+                      <SubLi key={idx}>- {item}</SubLi>
+                    ))}
+                  </SubUl>
+                </MainLi>
+                <MainLi>✨역할
+                  <SubUl>
+                    {item.role.split('/').map((item, idx) => (
+                      <SubLi key={idx}>- {item}</SubLi>
+                    ))}
+                  </SubUl>
+                </MainLi>
+                {
+                  item.note !== '' ? 
+                  ( 
+                    <MainLi>✨노트
+                      <SubUl>
+                        <SubLi>{item.note}</SubLi>
+                      </SubUl>
+                    </MainLi> 
+                  ) 
+                  : null
+                }
+                <MainLi>
+                  <a href={item.link} target='_blank'>✨보러가기!</a>
+                </MainLi>
+              </MainUl>
+            </Description>
+          </Section>
+        ))}
+      </Main>
     </>
   )
 }
