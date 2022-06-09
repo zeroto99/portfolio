c10e958d760f73b53992b49e2f245e47e271eafc
zeroto99 <myv_01@naver.com>
2022-06-10 00:19:13 +0900

    💄 반응형 분기점별 Container padding 속성 지정

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 1024px;
-  margin: 0 auto;
+  padding: 0 30rem;
+
+
+  @media ${(props) => props.theme.laptop} {
+    padding: 0 20rem;
+  }
+
+  @media ${(props) => props.theme.tablet} {
+    padding: 0 10rem;
+  }
+
+  @media ${(props) => props.theme.mobile} {
+    padding: 0 3rem;
+  }
 `;
