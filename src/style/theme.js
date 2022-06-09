7f73867d3ff4a25d2c23d552d307529f303196e5
zeroto99 <myv_01@naver.com>
2022-06-10 00:20:51 +0900

    💄 반응형 분기점 지정

---
+++ src/style/theme.js
@@ -0,0 +1,13 @@
+const size = {
+  mobile: '768px',
+  tablet: '1024px',
+  laptop: '1366px',
+};
+
+const theme = {
+  mobile: `(max-width: ${size.mobile})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+}
+
+export default theme;
