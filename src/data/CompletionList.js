c04f4c235047a79a10c47ede3057713a2b0c0cd2
zeroto99 <myv_01@naver.com>
2022-06-10 23:13:24 +0900

    📝 리스트 파일 추가

---
+++ src/data/CompletionList.js
@@ -0,0 +1,25 @@
+const CompletionList = [
+  {
+    'key' : '1',
+    'title' : '멋쟁이사자처럼 프론트엔드스쿨 1기 수료', 
+    'date' : '2021.10 ~ 2022.01',
+    'inf' : '주니어 프론트엔드 개발자 양성 과정', 
+    'inst' : '멋쟁이사자처럼'
+  },
+  {
+    'key' : '2',
+    'title' : '스마트기기 UI/UX 웹디자인(웹퍼블리셔) 수료', 
+    'date' : '2021.04 ~ 2021.08',
+    'inf' : 'HTML, CSS, JavaScript, jQuery 기초 학습', 
+    'inst' : '그린컴퓨터아카데미'
+  },
+  {
+    'key' : '3',
+    'title' : 'GTQ그래픽기술자격(포토샵) 1급', 
+    'date' : '2021.05.14',
+    'inf' : '', 
+    'inst' : '한국생산성본부'
+  },
+];
+
+export default CompletionList;
---
+++ src/data/ProjectList.js
@@ -0,0 +1,58 @@
+const ProjectList = [
+  { 
+    'key' : 1,
+    'title' : 'Reading Log', 
+    'img' : '/images/readinglog.jpg',
+    'date' : '2022.05',
+    'inf' : '카카오 책 검색 API를 활용하여 완독한 도서를 기록할 수 있는 웹페이지입니다. 책 제목을 검색하여 로그에 추가할 수 있습니다.',
+    'stack' : 'HTML(JSX), React, styled-components',
+    'way' : 'useState, useEffect 등 기본적인 React hooks를 통해 로직 재사용/useRef를 이용한 DOM제어/react-router-dom을 통한 라우팅/axios를 통한 비동기 처리 작업/styled-components를 이용한 스타일링',
+    'role' :'',
+    'note' : '',
+    'github': 'https://github.com/zeroto99/READING_LOG',
+    'link' :'https://zeroto99.github.io/READING_LOG/',
+  },
+  { 
+    'key' : 2,
+    'title' : '딸기마켓', 
+    'img' : '/images/sbmarket.jpg',
+    'date' : '2021.12 ~ 2022.02',
+    'inf' : `'멋쟁이사자처럼 프론트엔드 스쿨' 팀 과제로 진행된 프로젝트입니다. 제공되는 API와 Figma 디자인을 활용했습니다. 딸기라는 아이템을 선정하고 관련 상품을 판매하거나 구매할 수 있는 플랫폼입니다. 상품 등록 뿐만 아니라 피드, 게시물 작성, 팔로우 등의 기능도 구현하여 sns로 활용할 수 있습니다.`,
+    'stack' : 'HTML, CSS, JavaScript',
+    'way' : '팀원 전체의 수준을 고려하여 JavaScript로 진행/공통 페이지는 짝 프로그래밍(Pair Programming)을 통한 협업/async와 await를 통해 API 요청 비동기 처리 작업/try..catch를 통한 에러 핸들링/주간회의와 일일보고를 통해 지속적으로 진행 상황 공유 및 피드백',
+    'role' :'[공통] Splash, 로그인, 회원가입 페이지 구현/[개인] 게시글 작성 페이지, 게시글 상세 페이지, 검색 페이지 구현',
+    'note' : '',
+    'github': 'https://github.com/zeroto99/strawberry_market',
+    'link' :'https://strawberry-market.vercel.app',
+    'demo' : '(데모 아이디: hey@naver.com  비밀번호: 1234qwer)' 
+  },
+  { 
+    'key' : 3,
+    'title' : 'Flex & Grid', 
+    'img' : '/images/flexgrid.jpg',
+    'date' : '2022.05 ~ 진행 중',
+    'inf' : 'CSS 초심자가 flex와 grid를 배우는 데 도움을 주고자 기획된 오픈소스 프로젝트입니다. flex와 grid의 개념과 예제를 Cheat-Sheet처럼 찾아볼 수 있고 에디터를 통해 직접 코드를 작성해볼 수 있습니다.',
+    'stack' : 'HTML, CSS, JavaScript',
+    'way' : '지원자 참여률을 높이기 위해 JavaScript로 진행/공통 페이지는 짝 프로그래밍(Pair Programming)을 통한 협업/Figma 디자인 시스템 적용',
+    'role' :'[공통] 사이드 메뉴 드롭다운, 스크롤 스파이 기능 구현 /[개인] 사이드 메뉴 마크업, CSS 작업',
+    'note' : '',
+    'github': 'https://github.com/zeroto99/flexandgrid',
+    'link' :'https://flexngrid.com/',
+  },
+  { 
+    'key' : 4,
+    'title' : 'STOP WAR', 
+    'img' : '/images/stopwar.jpg',
+    'date' : '2022.03 ~ 2022.04',
+    'inf' : '우크라이나 전쟁과 관련된 소식을 제공하는 비영리 서비스입니다. 전쟁 관련 이슈를 항목별로 아카이빙하여 현재 상황과 후원 정보를 한 눈에 파악할 수 있습니다.',
+    'stack' : 'HTML, CSS, JavaScript',
+    'way' : '자원 봉사 참여률을 높이기 위해 JavaScript로 진행/짝 프로그래밍(Pair Programming)을 통한 협업/async와 await를 통한 비동기 처리 작업/try..catch를 통한 에러 핸들링',
+    'role' :'규제 페이지 구현',
+    'note' : '',
+    'github': 'https://github.com/zeroto99/stopwar',
+    'link' :'https://stopwar.co.kr',
+  },
+
+];
+
+export default ProjectList;
