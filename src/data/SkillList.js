af2cec37b01a557316bb72c0390d7d748cae3a8f
zeroto99 <myv_01@naver.com>
2022-06-10 23:17:54 +0900

    📝 SkillList 파일 추가

---
+++ src/data/SkillList.js
@@ -0,0 +1,44 @@
+const SkillList = [
+  {
+    'key' : '1',
+    'title' : 'HTML',
+    'state' : 'main',
+  },
+  {
+    'key' : '2',
+    'title' : 'CSS',
+    'state' : 'main',
+  },
+  {
+    'key' : '3',
+    'title' : 'JavsScript',
+    'state' : 'main',
+  },
+  {
+    'key' : '4',
+    'title' : 'React',
+    'state' : 'main',
+  },
+  {
+    'key' : '5',
+    'title' : 'Figma',
+    'state' : 'sub',
+  },
+  {
+    'key' : '6',
+    'title' : 'Zira',
+    'state' : 'sub',
+  },
+  {
+    'key' : '7',
+    'title' : 'Photoshop',
+    'state' : 'sub',
+  },
+  {
+    'key' : '8',
+    'title' : 'Illustrator',
+    'state' : 'sub',
+  },
+];
+
+export default SkillList;
